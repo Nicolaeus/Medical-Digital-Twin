2755d6870324b23a2968f8805aed4070b8e9ddb3
Nicolas <76745056+Nicolaeus@users.noreply.github.com>
2026-08-04 17:39:59 +0200

    Update Timeline.js

--- src/modules/timeline/Timeline.js
+++ src/modules/timeline/Timeline.js
@@ -1,1 +1,19 @@
+/**
+ * ==========================================================
+ * Medical Digital Twin
+ * TimelineModule.js
+ * ==========================================================
+ */
 
+export default class TimelineModule {
+
+    async render(root) {
+
+        root.innerHTML = `
+            <h1>Timeline</h1>
+            <p>Module en cours de développement.</p>
+        `;
+
+    }
+
+}
