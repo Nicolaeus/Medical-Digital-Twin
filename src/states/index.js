d54697f17144f5c48856bf0a08809c0986d4580b
Nicolas <76745056+Nicolaeus@users.noreply.github.com>
2026-08-04 14:43:10 +0200

    Create index.js

---
+++ src/states/index.js
@@ -0,0 +1,39 @@
+/**
+ * ==========================================================
+ * Medical Digital Twin
+ * state/index.js
+ * Global Application State
+ * ==========================================================
+ */
+
+import patient from "./patient.js";
+import health from "./health.js";
+import body from "./body.js";
+import environment from "./environment.js";
+import labs from "./labs.js";
+import timeline from "./timeline.js";
+import simulation from "./simulation.js";
+import settings from "./settings.js";
+import ui from "./ui.js";
+
+export default {
+
+    patient,
+
+    health,
+
+    body,
+
+    environment,
+
+    labs,
+
+    timeline,
+
+    simulation,
+
+    settings,
+
+    ui
+
+};
